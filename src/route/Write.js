7fd7280d7d60d053ace61811235080b5c351d436
fkrhkd507 <ourinon00@gmail.com>
2021-11-30 20:58:33 +0900

    wrtie popup add

--- src/route/Write.js
+++ src/route/Write.js
@@ -3,28 +3,34 @@ import '../css/Write.css';
 
 const Write = () => {
 
-
-
+    const style={
+        'display' : 'none'
+    }
     return (
-        <div className="write_wrap">
-            <div className="write_type">
-                <select className="page_type">
-                    <option>--- page type ---</option>
-                    <option>Html</option>
-                    <option>Css</option>
-                    <option>Javascript</option>
-                    <option>ReactJs</option>
-                    <option>NodeJs</option>
-                    <option>MySQL</option>
-                </select>
-            </div>
-            <div className="write_input">
-                <textarea>
-                
-                </textarea>
-            </div>
+        <div style={ style }>
+            <div className="write_modal">
+                <div className="write_wrap">
+                    <div className="write_type">
+                        <select className="page_type">
+                            <option>--- page type ---</option>
+                            <option>Html</option>
+                            <option>Css</option>
+                            <option>Javascript</option>
+                            <option>ReactJs</option>
+                            <option>NodeJs</option>
+                            <option>MySQL</option>
+                        </select>
+                    </div>
+                    <div className="write_input">
+                        <textarea>
+                        
+                        </textarea>
+                    </div>
 
-            <button className="save_button">Save</button>
+                    <button className="save_button">Save</button>
+                </div>
+            </div>
+            <div className="modal_bg"></div>
         </div>
     );
 }
